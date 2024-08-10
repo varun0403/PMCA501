e7950ab2c1fe9b63bde0261212d0bbf7553792b6
G Varun <89537125+varun0403@users.noreply.github.com>
2024-08-10 19:50:28 +0530

    Add files via upload

---
+++ queue_leetcode.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main(){
+	int arr[6] = {4,8,6,9,1,1},i,j,count,freq[6]={0,0,0,0,0,0};
+	queue<int>q;
+	for(i=0;i<6;i++){
+		for(j=i+1;j<6;j++){
+			if(arr[i] <= arr[j]){
+				count++;
+				freq[i] = count;
+				break;
+			}
+			else{
+				if(q.empty()){
+					q.push(arr[j]);
+					count++;
+				}
+				else{
+					if(arr[j] > q.back()){
+						count++;
+					}
+				}
+			}
+		}
+		while(!q.empty()){
+			q.pop();
+		}
+		freq[i] = count;
+		count = 0;
+	}
+	for(i=0;i<6;i++){
+		cout<<freq[i]<<endl;
+	}
+	return 0;
+}
