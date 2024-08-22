a7f4cfbd70d8b3bc866b9f838c2976b22d8aaa8c
G Varun <89537125+varun0403@users.noreply.github.com>
2024-08-22 23:46:39 +0530

    Update linked_list.cpp

--- linked_list.cpp
+++ linked_list.cpp
@@ -62,7 +62,17 @@ void pop_end(){
 }
 
 void pop_beg(){
-    head = head->next;
+    if(head == NULL){
+    	cout<<"Empty list!"<<endl;
+	}
+	else{
+		if(head->next == NULL){
+			head->next = NULL;
+		}
+		else{
+			head = head->next;
+		}
+	}
 }
 
 void traverse(){
@@ -144,27 +154,131 @@ void sort(){
 }
 
 void isDistinct(){
-    struct node *curr = head;
-    while(curr->next != NULL){
-        if(curr->val == curr->next->val){
-            cout<<"Not distinct"<<endl;
-            return;
-        }
-        curr = curr->next;
-    }
-    cout<<"distinct nodes"<<endl;
+	struct node *outer_node = head;
+	while(outer_node->next != NULL){
+		struct node *inner_node = outer_node->next;
+		while(inner_node != NULL){
+			if(outer_node->val == inner_node->val){
+				cout<<"Not distinct!"<<endl;
+				return;
+			}
+			inner_node = inner_node->next;
+		}	
+		outer_node = outer_node->next;
+	}
+	cout<<"Distinct list!"<<endl;
 }
 
-void minimum();
-void maximum();
-void sum();
-void avg();
+void minimum(){
+	if(head == NULL){
+		cout<<"Empty list! Cannot find minimum"<<endl;
+	}
+	else{
+		if(head->next == NULL){
+			cout<<"Minimum is "<<head->val<<endl;
+		}
+		else{
+			int min=head->val;
+			struct node *temp = head->next;
+			while(temp != NULL){
+				if(temp->val < min){
+					min = temp->val;
+				}
+				temp = temp->next;
+			}
+			cout<<"Minimum is "<<min<<endl;
+		}
+	}
+}
+void maximum(){
+	if(head == NULL){
+		cout<<"Empty list! Cannot find maximum"<<endl;
+	}
+	else{
+		if(head->next == NULL){
+			cout<<"Maximum is "<<head->val<<endl;
+		}
+		else{
+			int max=head->val;
+			struct node *temp = head->next;
+			while(temp != NULL){
+				if(temp->val > max){
+					max = temp->val;
+				}
+				temp = temp->next;
+			}
+			cout<<"Maximum is "<<max<<endl;
+		}
+	}
+}
+int sum(){
+	if(head == NULL){
+		cout<<"Empty list! Cannot summate"<<endl;
+		return 0;
+	}
+	else{
+		if(head->next == NULL){
+			return head->val;
+		}
+		else{
+			int s=0;
+			struct node *temp = head;
+			while(temp != NULL){
+				s = s + temp->val;
+				temp = temp->next;
+			}
+			return s;
+		}
+	}
+}
+void avg(){
+	if(head == NULL){
+		cout<<"Empty list! Cannot find average!"<<endl;
+	}
+	else{
+		if(head->next == NULL){
+			cout<<"Average is "<<head->val<<endl;
+		}
+		else{
+			int c = count();
+			int s = sum();
+			cout<<"Average is "<<s/c<<endl;
+		}
+	}
+}
+
+void remove(int data){
+	if(head == NULL){
+		cout<<"Empty list!"<<endl;
+		return;
+	}
+	else{
+		if(head->next == NULL){
+			if(head->val == data){
+				head = NULL;
+				return;
+			}
+		}
+		else{
+			struct node *temp = head;
+			while(temp != NULL){
+				if(temp->next->val == data){
+					temp->next = temp->next->next;
+					return;
+				}
+				temp = temp->next;
+			}
+		}
+	}
+}
+
 void insert_mid();
 void pop_mid();
 void sliding_window();
 
 int main(){
     cout<<"1)Push at End 2)Push at Beg 3)Pop at end 4)Pop at beg 5)Traverse 6)Count 7)Search 8)Sort"<<endl;
+    cout<<"9)Minimum 10)Maximum 11)Sum 12)Average 13)Distinct 14)Remove"<<endl;
     int c,val;
     bool s;
     while(true){
@@ -203,6 +317,29 @@ int main(){
             case 8:
                 sort();
                 break;
+            case 9:
+            	minimum();
+            	break;
+            case 10:
+            	maximum();
+            	break;
+            case 11:
+            	val = sum();
+            	cout<<"The sum is "<<val<<endl;
+            	break;
+            case 12:
+            	avg();
+            	break;
+            case 13:
+            	isDistinct();
+            	break;
+            case 14:
+            	cout<<"Enter val to be removed: ";
+            	cin>>val;
+            	remove(val);
+            	break;
+            default:
+            	return -1;
         }
     }
     return 0;
