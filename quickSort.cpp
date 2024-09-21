c3ed2003bc50a698d2a88250b02f68b40f770fac
G Varun <89537125+varun0403@users.noreply.github.com>
2024-09-21 18:13:32 +0530

    Update quickSort.cpp

--- quickSort.cpp
+++ quickSort.cpp
@@ -1,51 +1,47 @@
 #include<iostream>
 using namespace std;
 
-int partition(int arr[], int low, int high){
-    int pivot = high;
-    int left = low;
-    int right = high - 1;
-
-    while (left <= right) {
-        if (arr[left] <= arr[pivot]){
-            left++;
-        }
-        else{
-        	if(arr[right] > arr[pivot]){
-        		right--;
-			}
-			else{
-				swap(arr[left],arr[right]);
-			}
+int partition(int arr[],int low,int high){
+	int pivot = high;
+	int left = 0, right = high - 1;
+	int mid = (high - low) / 2 + low;
+	swap(arr[mid],arr[pivot]);
+	
+	while(left <= right){
+		if(arr[left] <= arr[pivot]){
+			left++;
+		}
+		else if(arr[right] > arr[pivot]){
+			right--;
+		}
+		else{
+			swap(arr[left],arr[right]);
 		}
 	}
-    swap(arr[left], arr[pivot]);
-    return left; //return pos of pivot
+	swap(arr[left],arr[pivot]);
+	return left;
 }
 
-void quickSort(int arr[], int low, int high){
-    if (low < high) {
-        int pi = partition(arr, low, high); //partition the array
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
+int quickSort(int arr[],int low,int high){
+	if(low < high){
+		int pi = partition(arr,low,high);
+		quickSort(arr,low,pi-1);
+		quickSort(arr,pi+1,high);
+	}
 }
 
-void print_arr(int arr[], int size){
-    for (int i = 0; i < size; i++) {
+void print_arr(int arr[], int n){
+    for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
     cout << endl;
 }
 
 int main(){
-    int arr[5] = {5, 4, 3, 2, 1};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    cout<<"Array before sorting: ";
-    print_arr(arr,size);
-    quickSort(arr, 0, size - 1);
-    cout<<"\nArray after sorting: ";
-    print_arr(arr, size);      
-    return 0;
+	int nums[] = {5,4,3,2,1};
+	int low = 0;
+	int high = sizeof(nums)/sizeof(nums[0]);
+	quickSort(nums,low,high-1);
+	print_arr(nums,high);
+	return 0;
 }
-
