6d5e48de48a7d666be5588a38f33b83ae6e89e2b
G Varun <89537125+varun0403@users.noreply.github.com>
2024-09-23 23:58:48 +0530

    Update sorting_algorithms.cpp

--- sorting_algorithms.cpp
+++ sorting_algorithms.cpp
@@ -2,48 +2,53 @@
 using namespace std;
 
 void bubble_sort(int arr[],int n){
-  	int i,j,temp;
-  	for(i=0; i<n; i++){
-    		for(j=0; j<n-1; j++){
-      			if(arr[j] > arr[j+1]){
-        				temp = arr[j];
-        				arr[j] = arr[j+1];
-        				arr[j+1] = temp;
-      			}
-    		}
-	}
-	for(i=0;i<n;i++){
-		  cout<<arr[i]<<" ";
+	int i,j,temp;
+	for(i=0; i<n; i++){
+		for(j=0; j<n-1; j++){
+			if(arr[j] > arr[j+1]){
+				temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
 	}
 }
 
 void selection_sort(int arr[], int n){
-  	int i,j,min_index,temp;
-  	for(i=0; i<n; i++){
-    		min_index = i;
-    		for(j=i+1; j<n; j++){
-      			if(arr[j] < arr[min_index]){
-      				  min_index = j;
-      			}
-    		}
-    		temp = arr[min_index];
-    		arr[min_index] = arr[i];
-    		arr[i] = temp;
-  	}
-  	for(i=0;i<n;i++){
-  		  cout<<arr[i]<<" ";
-  	}
+	int i,j,min_index,temp;
+	for(i=0; i<n; i++){
+		min_index = i;
+		for(j=i+1; j<n; j++){
+			if(arr[j] < arr[min_index]){
+				min_index = j;
+			}
+		}
+		if (i != min_index){
+			temp = arr[min_index];
+			arr[min_index] = arr[i];
+			arr[i] = temp;
+		}
+	}
 }
 
-void isSorted(int arr[], int n){
-	int i,j;
-	for(i=0;i<n-1;i++){
-		  if(arr[i] > arr[i+1]){
-			    cout<<"Not sorted!"<<endl;
-			    return;
-		  }
+void insertion_sort(int arr[], int n){
+	for(int i=1;i<n;i++){
+		int key = arr[i];
+		int j = i-1;
+		while(j>=0 && arr[j]>key){
+			arr[j+1] = arr[j];
+			j--;
+		}
+		arr[j+1] = key;
 	}
-	cout<<"Sorted!"<<endl;
+}
+
+
+void print_array(int arr[],int n){
+	for(int i=0;i<n-1;i++){
+		cout<<arr[i]<<" ";
+	}
+	cout<<arr[n-1]<<endl;
 }
 
 int main(){
@@ -52,8 +57,9 @@ int main(){
 	cin>>size;
 	int arr[size];
 	for(int i=0; i<size; i++){
-		  cin>>arr[i];
+		cin>>arr[i];
 	}
-	isSorted(arr,size);
+	insertion_sort(arr,size);
+	print_array(arr,size);
 	return 0;
 }
