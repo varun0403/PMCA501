cc58dd9fa88d24e7c1b779e329c2fd4bce5eaadc
G Varun <89537125+varun0403@users.noreply.github.com>
2024-11-19 08:16:06 +0530

    Update queue_linked_list.cpp

--- queue_linked_list.cpp
+++ queue_linked_list.cpp
@@ -1,117 +1,58 @@
-#include <iostream>
+#include<iostream>
+#include<malloc.h>
 using namespace std;
 
-struct node {
-    int data;
-    struct node* next;
+struct node{
+	int data;
+	struct node *next;
 };
 
-struct node* front = NULL;
-struct node* rear = NULL;
-
-bool isEmpty(){
-    return front == NULL;
+struct node *createNode(int val){
+	struct node *new_node = (struct node*) malloc(sizeof(struct node));
+	new_node->data = val;
+	new_node->next = NULL;
+	return new_node;
 }
 
-void push(int val){
-    struct node* new_node = (struct node*) malloc(sizeof(struct node));
-    new_node->data = val;
-    new_node->next = NULL;
-    if (isEmpty()){
-        front = new_node;
-        rear = new_node;
-    }
-    else{
-        if(front == rear){
-            rear = new_node;
-            front->next = rear;
-        }
-        else{
-            struct node* current = front;
-            while(current->next != NULL){
-                    current = current->next;
-            }
-            rear = new_node;
-            current->next = new_node;
-        }
-    }
+void enqueue(struct node *&root,int val){
+	if(root == NULL){
+		root = createNode(val);
+	}
+	else{
+		struct node *temp = root;
+		while(temp->next != NULL){
+			temp = temp->next;
+		}
+		temp->next = createNode(val);
+	}
 }
 
-void pop(){
-    if(isEmpty()){
-        cout<<"Empty list!";
-    }
-    else{
-        if(front == rear){
-            front = NULL;
-            rear = NULL;
-	    free(front);
-            free(rear);
-        }
-        else{
-            front = front->next;
-        }
-    }
+void dequeue(struct node *&root){
+	if(root == NULL){
+		cout << "Empty queue!" << endl;
+	}
+	else{
+		root = root->next;
+	}
 }
 
-int peek(){
-    if(isEmpty()){
-        return -1;
-    }
-    return front->data;
-}
-
-void display(){
-    struct node* temp = front;
-    if(front == rear){
-        cout<<front->data<<endl;
-    }
-    else{
-        while(temp != NULL){
-            cout<<temp->data<<"->";
-            temp = temp->next;
-        }
-        cout<<"NULL"<<endl;
-    }
+void peek(struct node *root){
+	if(root == NULL){
+		cout << "Empty queue!" << endl;
+	}
+	else{
+		cout << "QUEUE[FRONT] : " << root->data;
+	}
 }
 
 int main(){
-    cout<<"1)Push 2)Pop 3)Peek 4)Display 5)Exit"<<endl;
-	int inp,temp,data;
-	while(true){
-		cout<<"\nEnter your choice: ";
-		cin>>inp;
-		switch(inp){
-			case 1:
-				cout<<"Enter value: ";
-				cin>>data;
-				push(data);
-				break;
-				
-			case 2:
-				pop();
-				break;
-				
-			case 3:
-				temp = peek();
-				if(temp == -1){
-					cout<<"Empty queue!"<<endl;
-				}
-				else{
-					cout<<temp<<endl;
-				}
-				break;
-				
-			case 4:
-			    	display();
-			    	break;
-			case 5:
-				return 0;
-				break;
-				
-			default:
-				cout<<"Retry"<<endl;
-		}
-	}
+	struct node *q = NULL;
+	enqueue(q,12);
+	enqueue(q,15);
+	enqueue(q,16);
+	enqueue(q,19);
+	peek(q);
+	dequeue(q);
+	peek(q);
 	return 0;
 }
