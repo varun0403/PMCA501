42bd28a637d2a5ef5b80dedaba590916d7f210e5
G Varun <89537125+varun0403@users.noreply.github.com>
2024-11-19 08:06:38 +0530

    Update stack_linked_list.cpp

--- stack_linked_list.cpp
+++ stack_linked_list.cpp
@@ -3,116 +3,64 @@
 using namespace std;
 
 struct node{
-	int val;
-	struct node* next;
+	int data;
+	struct node *next;
 };
 
-struct node* head = NULL;
-struct node* top = NULL;
-int count = 0;
-
-bool isEmpty(){
-	return top == NULL;
-}
-
-void push(int data){
-	struct node* new_node = (struct node*)malloc(sizeof(struct node));
+struct node *createNode(int val){
+	struct node *new_node = (struct node*) malloc(sizeof(struct node));
+	new_node->data = val;
 	new_node->next = NULL;
-	new_node->val = data;
-	if(isEmpty()){
-		head = new_node;
-		top = new_node;
+	return new_node;
+}
+
+void push(struct node *&root,int val){
+	if(root == NULL){
+		root = createNode(val);
 	}
 	else{
-		struct node* current = head;
-		while (current->next != NULL) {
-		    current = current->next;
+		struct node *temp = root;
+		while(temp->next != NULL){
+			temp = temp->next;
 		}
-		current->next = new_node;
-		top = new_node;
+		temp->next = createNode(val);
 	}
 }
 
-void pop() {
-    if (!isEmpty()){
-        if (head == top){
-            head = NULL;
-            top = NULL;
-        } 
-	else {
-            struct node* current = head;
-            while (current->next->next != NULL){
-                current = current->next;
-            }
-            free(current->next);
-            current->next = NULL;
-            top = current;
-        }
-    } 
-    else {
-        cout<<"Stack is empty!"<<endl;
-    }
-}
-
-int peek(){
-	if(isEmpty()){
-		return 0;
-	}
-	return top->val;
-}
-
-void display(){
-	if(head == NULL){
-		cout<<"Empty!";
+void pop(struct node *&root){
+	if(root == NULL){
+		cout << "Empty stack!" << endl;
 	}
 	else{
-		struct node* current = head;
-		while(current->next != NULL){
-			cout<<current->val<<" -> ";
-			current = current->next;
+		struct node *temp = root;
+		while(temp->next->next != NULL){
+			temp = temp->next;
 		}
-		cout<<current->val;
+		temp->next = NULL;
+	}
+}
+
+void peek(struct node *root){
+	if(root == NULL){
+		cout << "Empty stack!" << endl;
+	}
+	else{
+		struct node *temp = root;
+		while(temp->next != NULL){
+			temp = temp->next;
+		}
+		cout << "STACK[TOP] : " << temp->data << endl;
 	}
 }
 
 int main(){
-	cout<<"1)Push 2)Pop 3)Peek 4)Display 5)Exit"<<endl;
-	int inp,temp,data;
-	while(true){
-		cout<<"\nEnter your choice: ";
-		cin>>inp;
-		switch(inp){
-			case 1:
-				cout<<"Enter value: ";
-				cin>>data;
-				push(data);
-				break;
-				
-			case 2:
-				pop();
-				break;
-				
-			case 3:
-				temp = peek();
-				if(temp == 0){
-					cout<<"Empty list!"<<endl;
-				}
-				else{
-					cout<<temp<<endl;
-				}
-				break;
-				
-			case 4:
-				display();
-				break;
-			
-			case 5:
-				return 0;
-				break;
-				
-			default:
-				cout<<"Retry"<<endl;
-		}
-	}
+	struct node *stack = NULL;
+	push(stack,12);
+	push(stack,16);
+	push(stack,17);
+	push(stack,18);
+	peek(stack);
+	pop(stack);
+	peek(stack);
 	return 0;
 }
